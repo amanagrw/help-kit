0aab9b6305fa9ebc24e8e62d5a10c3bd3f6a466d
Archit Kaushik <asarchit65@gmail.com>
2019-03-19 10:09:26 +0530

    Changed the group/join route

--- Server/Server.js
+++ Server/Server.js
@@ -75,28 +75,29 @@ app.get("/group/:id", (req, res) => {
 app.get(
   "/group/join/:id",
   passport.authenticate("jwt", { session: false }),
-  (req, res) => {
+  async (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
       return res.status(404).send();
     }
-
-    Group.findById(id)
-      .then(group => {
-        group.members.forEach(memberId => {
-          if (memberId.equals(req.user._id)) {
-            return res.send("user already a member");
-          }
+    try {
+      let group = await Group.findById(id);
+      let isMember = group.members.some(memberId =>
+        memberId.equals(req.user._id)
+      );
+      if (isMember) {
+        return res.send("user already a member");
+      }
+      group.members.push(req.user._id);
+      group.save().then(group => {
+        req.user.groups.push(group._id);
+        req.user.save().then(user => {
+          res.send(user.groups);
         });
-        group.members.push(req.user._id);
-        group.save().then(group => {
-          req.user.groups.push(group._id);
-          req.user.save().then(user => {
-            res.send(user.groups);
-          });
-        });
-      })
-      .catch(e => res.status(400).send(e));
+      });
+    } catch (e) {
+      res.status(400).send(e);
+    }
   }
 );
 
